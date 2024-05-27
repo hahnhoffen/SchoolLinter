f871b1c94a517c8bcbcc913605d18e19f78111d1
Tim Hahn Colbro <117572147+hahnhoffen@users.noreply.github.com>
2024-05-27 10:30:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const x = 5
 const y = 7
 function double (x) { return x * 2 }
-console.print('Dubbelt av 5 är ' + double(x));
-console.print('Dubbelt av 7 är ' + double(y));
+console.print('Dubbelt av 5 är ' + double(x))
+console.print('Dubbelt av 7 är ' + double(y))
